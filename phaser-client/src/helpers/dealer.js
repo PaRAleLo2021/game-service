fc53f3948a456cb49b743e8da97e2d80b2ac3972
RenyyyK <reny_kulcsar@yahoo.com>
2021-05-05 19:55:49 +0300

    Get random cards for 2 players - not unused cards only

--- phaser-client/src/helpers/dealer.js
+++ phaser-client/src/helpers/dealer.js
@@ -3,18 +3,23 @@ import Card from './card';
 export default class Dealer {
     constructor(scene) {
         this.dealCards = () => {
-            let playerSprite;
-            let opponentSprite;
-            if (scene.isPlayerA) {
-                playerSprite = 'card_1';
-                opponentSprite = 'card_0';
-            } else {
-                playerSprite = 'card_2';
-                opponentSprite = 'card_0';
-            };
+            let opponentSprite = 'card_0';
+            let usedCards = new Array(36);
+            
             for (let i = 0; i < 5; i++) {
                 let playerCard = new Card(scene);
-                playerCard.render(475 + (i * 100), 650, playerSprite);
+
+                /*let usableCards = new Array(36);
+                for (let j = 1; j < 37; j++) {
+                    if (!usedCards.includes(j))
+                        usableCards.push(j);
+                } */
+
+                //let randomNumber = getRandomIntInclusive(1, usableCards.length);
+                let cardNumber = getRandomIntInclusive(1, 36);  //parseInt(usableCards[randomNumber]);
+                
+                playerCard.render(475 + (i * 100), 650, 'card_' + cardNumber);
+                usedCards.push(cardNumber);
 
                 let opponentCard = new Card(scene);
                 scene.opponentCards.push(opponentCard.render(475 + (i * 100), 125, opponentSprite).disableInteractive());
@@ -22,3 +27,10 @@ export default class Dealer {
         }
     }
 }
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+  
